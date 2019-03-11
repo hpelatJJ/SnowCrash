8c3275b4501a827183222f4557fd688ba4879b80
Kamel HABBAR <khabbar@e2r4p19.42.fr>
2019-03-11 11:56:51 +0100

    level09 flag and explainations

---
+++ level09/script.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+
+int    main(int ac, char **av)
+{
+	int    fd = open(av[1], O_RDONLY);
+	char    buff[1024];
+	int r = read(fd, &buff, 1024);
+	int    i = 0;
+	while(i < r - 1)
+	{
+		printf("%c", buff[i] - i);
+		i++;
+	}
+	return (0);
+}
